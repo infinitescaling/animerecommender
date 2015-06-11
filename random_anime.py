67a5ea54d21133e5ebd6b501cbe8fa9e55784038
infinitescaling <shogo564@gmail.com>
2015-06-11 11:00:21 -0500

    fixed unfound titles

--- random_anime.py
+++ random_anime.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import requests
 import re
 import random
@@ -19,7 +20,7 @@ def getXML():
 def pick_random():
     getXML()
     match = re.findall('>([^"<"]+)</anime>', anime_xml)
-    match = [s.encode('ascii','ignore') for s in match]
+    match = [s.encode('utf8','ignore') for s in match]
     match = [s.replace('&amp;', '&') for s in match]
     return random.choice(match)
     
@@ -27,12 +28,18 @@ def get_summary(anime_title):
     global anime_xml
     if "(" in anime_title:
         anime_title = anime_title[:anime_title.index("(")]
+    if "-" in anime_title:
+        anime_title = anime_title[:anime_title.index("-")]
     reg_expression = '<anime href="([^"]+)">'+ anime_title
     match = re.findall(reg_expression, anime_xml)
+    if not match:
+        return get_summary(pick_random())
     request = requests.get("http://www.animenewsnetwork.com"+match[0])
     anime_info = request.text
     reg_expression = "<strong>Plot Summary:</strong> \n\t<span>([^<]+)</span>"
     match = re.findall(reg_expression, anime_info)
     match = [s.encode('ascii', 'ignore') for s in match]
+    if not match:
+        return "No summary at this time"
     return match[0]
 
